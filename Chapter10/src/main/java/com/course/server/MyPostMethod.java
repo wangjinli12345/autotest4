dd77390aa71d9de33ea9ad5f135bf4b785cd575a
wagnjinli <17609932037@163.com>
2019-09-23 13:59:00 +0800

    第十一章有变更

--- Chapter10/src/main/java/com/course/server/MyPostMethod.java
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -1,14 +1,13 @@
 package com.course.server;
 
 
+import com.course.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -31,6 +30,32 @@ public class MyPostMethod {
             return "恭喜你登录成功了！";
         }
         return "用户名或者密码错误！";
+
+    }
+
+    @RequestMapping(value = "/getUserList",method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表",httpMethod = "POST")
+    public String getUserList(HttpServletRequest request,
+                            @RequestBody User u){
+        User user;
+        //获取cookies
+        Cookie[] cookies =request.getCookies();
+        //验证cookies是否合法
+        for (Cookie c :cookies){
+            if (c.getName().equals("login")
+                    && c.getValue().equals("true")
+                    && u.getUserName().equals("zhangsan")
+                    && u.getPassword().equals("123456")
+            ){
+                user=new User();
+                user.setName("lisi");
+                user.setAge("18");
+                user.setSex("man");
+                return user.toString();
+
+            }
+        }
+        return "参数不合法";
 
     }
 }
--- Chapter10/src/main/java/hello/SampleController.java
+++ Chapter10/src/main/java/hello/SampleController.java
@@ -1,21 +1,21 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+
     @RequestMapping("/")
     @ResponseBody
-    String home(){
-        return "Hello Word";
+    String home() {
+        return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception{
-        SpringApplication.run(SampleController.class,args);
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
     }
 }
